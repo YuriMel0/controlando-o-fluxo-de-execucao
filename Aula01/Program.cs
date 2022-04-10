8e5a25f11d3c11f5f26ac5c3ba12f39fbcd06216
Yuri Mel0 <meloy316@gmail.com>
2022-04-10 12:38:10 -0300

    adicionando exemplo da aula01 e arquivos para debug da aplicação

--- Aula01/Program.cs
+++ Aula01/Program.cs
@@ -6,7 +6,20 @@ namespace Aula01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int primeiroValor;
+            int segundoValor;
+            int resultadoSoma;
+
+            Console.WriteLine("Informe o primeiro valor: ");
+            primeiroValor = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o segundo valor: ");
+            segundoValor = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O primeiro valor digitado foi {primeiroValor}");
+            Console.WriteLine($"O segudno valor digitado foi {segundoValor}");
+
+            resultadoSoma = primeiroValor + segundoValor;
+            Console.WriteLine($"O resultado da soma entre os valores é: {resultadoSoma}");
         }
     }
 }
