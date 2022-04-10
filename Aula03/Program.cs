3095f46427022441bafa3176cd9ae720ead5cd9f
Yuri Mel0 <meloy316@gmail.com>
2022-04-10 16:31:52 -0300

    adicionando exemplos da aula03 e arquivos para debug

--- Aula03/Program.cs
+++ Aula03/Program.cs
@@ -6,7 +6,65 @@ namespace Aula03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            quartoExemplo();
+        }
+
+        static void primeiroExemplo()
+        {
+            int numero;
+            Boolean condicao = true;
+
+            while (condicao)
+            {
+                Console.WriteLine(@"Informe um númmero, 0 para sair: ");
+                numero = int.Parse(Console.ReadLine());
+
+                if (numero == 0)
+                {
+                    condicao = false;
+                }
+                Console.WriteLine($"O número digitado foi {numero}");
+            }
+        }
+
+        static void segundoExemplo()
+        {
+            int numero;
+            Boolean condicao = true;
+
+            do
+            {
+                Console.WriteLine(@"Informe um númmero, 0 para sair: ");
+                numero = int.Parse(Console.ReadLine());
+
+                if (numero == 0)
+                {
+                    condicao = false;
+                }
+                Console.WriteLine($"O número digitado foi {numero}");
+            } while (condicao);
+        }
+
+        static void terceiroExemplo()
+        {
+            int numero;
+
+            Console.WriteLine("Informe um número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numero; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void quartoExemplo()
+        {
+            int[] lista = {1, 2, 3, 4, 5};
+            foreach (int numero in lista)
+            {
+                Console.WriteLine(numero);
+            }
         }
     }
 }
